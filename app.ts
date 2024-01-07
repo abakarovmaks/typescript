0cc56ebf45b39755b775924987a5f39013fe04ae
abakarovmaks <abakarovmaks@gmail.com>
2024-01-07 15:09:29 +0200

    working with Numbers, Strings & Booleans

--- app.ts
+++ app.ts
@@ -1,11 +1,15 @@
-const button = document.querySelector('button')! as HTMLButtonElement;
-const input1 = document.getElementById('num1')! as HTMLInputElement;
-const input2 = document.getElementById('num2')! as HTMLInputElement;
-
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+  const result = num1 + num2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
-button.addEventListener('click', function () {
-  console.log(add(+input1.value, +input2.value));
-});
+const number1 = 5;
+const number2 = 3.2;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+add(number1, number2, printResult, resultPhrase);
