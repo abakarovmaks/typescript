ded207bd7d757287f7a964b6a5d46e069ce8f2c8
abakarovmaks <abakarovmaks@gmail.com>
2024-01-07 22:25:17 +0200

    add enum

--- app.ts
+++ app.ts
@@ -1,8 +1,30 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Maxym',
+//   age: 99,
+//   hobbies: ['Sports', 'Cookies'],
+//   role: [2, 'author'],
+// };
+
+enum Role {
+  ADMIN,
+  ONLY_READ_USER,
+  AUTHOR,
+}
+
 const person = {
   name: 'Maxym',
   age: 99,
   hobbies: ['Sports', 'Cookies'],
+  role: Role.ADMIN,
 };
+
+// person.role.push('admin');
+// person.role[1] = '10';
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports', 'Work'];
@@ -11,4 +33,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
 }
