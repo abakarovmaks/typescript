c9112587e536b240d6ad3abb04990e732862a380
abakarovmaks <abakarovmaks@gmail.com>
2024-01-09 22:42:07 +0200

    literal types

--- app.ts
+++ app.ts
@@ -1,15 +1,25 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (
+    (typeof input1 === 'number' && typeof input2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combinedAges = combine(30, 28);
+const combinedAges = combine(30, 28, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Maksym', 'Vitalina');
+const combinedStringAges = combine('30', '12', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Maksym', 'Vitalina', 'as-text');
 console.log(combinedNames);
