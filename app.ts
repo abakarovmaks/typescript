23ef5ef089743c5683f8e7e7f13a86979268636f
abakarovmaks <abakarovmaks@gmail.com>
2024-01-11 22:00:49 +0200

    functions and callbacks

--- app.ts
+++ app.ts
@@ -6,4 +6,20 @@ function printResult(num: number) {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 2));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+
+console.log(combineValues(7, 7));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
