23aa6d8be01a99dcf5d0715266e20d46f59be61e
abakarovmaks <abakarovmaks@gmail.com>
2024-01-16 12:34:53 +0200

    add private methods

--- src/app.ts
+++ src/app.ts
@@ -15,16 +15,29 @@
 
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n
   }
 
   describe(this: Department) {
-    console.log('Department: ',this.name);
-    
+    console.log('Department: ',this.name); 
+  }
+  addEmployee(employee: string) {
+    this.employees.push(employee) 
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length); 
+    console.log(this.employees); 
   }
 }
 const accounting = new Department('Accounting')
+accounting.addEmployee('Max')
+accounting.addEmployee('Vita')
+
+// accounting.employees[2] = 'Anna'
+
 accounting.describe()
+accounting.printEmployeeInformation()
 
