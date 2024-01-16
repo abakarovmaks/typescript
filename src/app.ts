a98ab5ebd92330eba9b47b0fe5424d05ad6df0e4
abakarovmaks <abakarovmaks@gmail.com>
2024-01-16 13:36:36 +0200

    getter and setter

--- src/app.ts
+++ src/app.ts
@@ -47,11 +47,29 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport
+    }
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass a valid value");
+    }
+    this.addReport(value)
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting')
+    this.lastReport = reports[0]
   }
   addReport(text: string) {
     this.reports.push(text)
+    this.lastReport = text;
   }
   getReports() {
     console.log(this.reports);
@@ -78,7 +96,9 @@ it.printEmployeeInformation()
 console.log(it);
 
 const accounting = new AccountingDepartment('d2', []);
+accounting.mostRecentReport = 'Annual report';
 accounting.addReport('Something went wrong...')
+console.log(accounting.mostRecentReport);
 
 accounting.addEmployee('Maxu')
 accounting.addEmployee('Manu')
