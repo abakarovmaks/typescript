78cb231f6e6e177e14d65f4bfd50b099a719115d
abakarovmaks <abakarovmaks@gmail.com>
2024-01-19 10:52:50 +0200

    type guards

--- src/app.ts
+++ src/app.ts
@@ -22,3 +22,57 @@ type Combinable2 = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable2 & Numeric;
+
+function addCombinable(a: Combinable2, b: Combinable2) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString()
+    }
+    return a + b
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privilege: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start date:  ' + emp.startDate);
+    }
+}
+
+printEmployeeInformation(e1)
+printEmployeeInformation({ name: 'Manu', startDate: new Date() })
+
+class Car {
+    drive() {
+        console.log('Driving...');
+    };
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    };
+
+    loadCargo(amount: number) {
+        console.log('Loading truck...' + amount);
+        
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive()
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(2000)
+    }
+}
+
+useVehicle(v1)
+useVehicle(v2)
