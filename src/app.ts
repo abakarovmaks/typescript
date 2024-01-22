8fb90c6a015c04a41e6cfe701710c0da7d9d33fd
abakarovmaks <abakarovmaks@gmail.com>
2024-01-22 10:38:51 +0200

    type casting

--- src/app.ts
+++ src/app.ts
@@ -100,8 +100,11 @@ function moveAnimal(animal: Animal) {
     
         default:
             break;
-    }
+        }
     console.log('Moving at speed: ' + speed);
 }
+moveAnimal({ type: "bird", flyingSpeed: 10 })
 
-moveAnimal({type: "bird", flyingSpeed: 10})
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!
+const userInputElement = <HTMLInputElement>document.getElementById('user-input') as HTMLInputElement;
+userInputElement.value = 'Hi there!'
