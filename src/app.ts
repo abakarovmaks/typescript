3231e80fa5b99f7451f47cf2065f4971dba52cad
abakarovmaks <abakarovmaks@gmail.com>
2024-01-23 13:46:33 +0200

    generic classes

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,43 @@ function countAndDescribe<T extends Lenghty>(element: T): [T, string] {
 }
 console.log(countAndDescribe('Hi there!'));
 
+function extractAndConvert<T extends Object, U extends keyof T>(obj: T, key: U) {
+    return 'Value: ' + obj[key]
+}
+extractAndConvert({name: 'Max'}, 'name')
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = []
+
+    addItem(item: T) {
+        this.data.push(item)
+    }
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return
+        }
+        this.data.splice(this.data.indexOf(item), 1)
+    }
+    getItems() {
+        return [...this.data]
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max')
+textStorage.addItem('Manu')
+textStorage.removeItem('Max')
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objectStorage = new DataStorage<object>();
+// objectStorage.addItem({name: 'Max'})
+// objectStorage.addItem({name: 'Manu'})
+// objectStorage.removeItem({ name: 'Max' })
+// console.log(objectStorage.getItems());
+
+
 
 
 
