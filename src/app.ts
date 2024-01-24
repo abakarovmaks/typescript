cd9057afb697fb004912798d112a721ec9eb21ba
abakarovmaks <abakarovmaks@gmail.com>
2024-01-24 13:48:10 +0200

    decorators

--- src/app.ts
+++ src/app.ts
@@ -1,89 +1,30 @@
-// const names: Array<string> = [];
-// // names[0].split(' ')
-
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve('This is done!')
-//     }, 2000);
-// })
-
-// promise.then(data => {
-//     console.log(data.split(' '));
-// })
-
-function merge<T extends Object, U>(objA: T, objB: U) {
-    return Object.assign(objA, objB)
-}
-const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 31 });
-console.log(mergeObj.age);
-
-interface Lenghty {
-    length: number;
+function Logger(logString: string) {
+    return function (constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    } 
 }
 
-function countAndDescribe<T extends Lenghty>(element: T): [T, string] {
-    let descriptiontext = 'Got no value.'
-    if (element.length === 1) {
-        descriptiontext = 'Got 1 element.'
-    } else if (element.length > 1) {
-        descriptiontext = 'Got ' + element.length + ' elements.'
-    }
-    return [element, descriptiontext]
-}
-console.log(countAndDescribe('Hi there!'));
-
-function extractAndConvert<T extends Object, U extends keyof T>(obj: T, key: U) {
-    return 'Value: ' + obj[key]
-}
-extractAndConvert({name: 'Max'}, 'name')
-
-class DataStorage<T extends string | number | boolean> {
-    private data: T[] = []
-
-    addItem(item: T) {
-        this.data.push(item)
-    }
-    removeItem(item: T) {
-        if (this.data.indexOf(item) === -1) {
-            return
+function WithTemplate(template: string, hookId: string) {
+    return function (constructor: any) {
+        const hookEl = document.getElementById(hookId)
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = p.name
         }
-        this.data.splice(this.data.indexOf(item), 1)
-    }
-    getItems() {
-        return [...this.data]
     }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem('Max')
-textStorage.addItem('Manu')
-textStorage.removeItem('Max')
-console.log(textStorage.getItems());
+// @Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My person object</h1>', 'app')
+class Person2 {
+    name = 'Max';
 
-const numberStorage = new DataStorage<number>();
-
-// const objectStorage = new DataStorage<object>();
-// objectStorage.addItem({name: 'Max'})
-// objectStorage.addItem({name: 'Manu'})
-// objectStorage.removeItem({ name: 'Max' })
-// console.log(objectStorage.getItems());
-
-interface CourseGoal {
-    title: string,
-    description: string,
-    completeUntil: Date
+    constructor() {
+        console.log('Creating person pbject...');
+    }
 }
 
-function createCourseGoal(title: string, description: string, date: Date ): CourseGoal {
-    // return {title: title, description: description, completeUntil: date }
-    let courseGoal: Partial<CourseGoal> = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntil = date;
-    return courseGoal as CourseGoal;
-}
-
-const names: Readonly<string[]> = ['Max', 'Anna'];
-// names.push('Manu');
-
-
+const pers = new Person2();
+console.log(pers);
