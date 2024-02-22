8d0d62cc5e58055bea0d249e86d57345deae9c98
abakarovmaks <abakarovmaks@gmail.com>
2024-02-22 15:51:41 +0200

    autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,38 @@
 function Logger(logString: string) {
-    console.log('LOGGER FACTORY');
-    
-    return function (constructor: Function) {
-        console.log(logString);
-        console.log(constructor);
-    } 
+  console.log('LOGGER FACTORY');
+
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
 function WithTemplate(template: string, hookId: string) {
-    return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {
-        return class extends originalConstructor {
-            constructor(...args: any[]) {
-                super()
-                console.log('TEMPLATE FACTORY');
-                const hookEl = document.getElementById(hookId)
-                if (hookEl) {
-                    hookEl.innerHTML = template;
-                     hookEl.querySelector('h1')!.textContent = this.name
-                }
-            }
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(...args: any[]) {
+        super();
+        console.log('TEMPLATE FACTORY');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
         }
-    }
+      }
+    };
+  };
 }
 
 @Logger('LOGGING - PERSON')
 @WithTemplate('<h1>My person object</h1>', 'app')
 class Person2 {
-    name = 'Max';
+  name = 'Max';
 
-    constructor() {
-        console.log('Creating person pbject...');
-    }
+  constructor() {
+    console.log('Creating person pbject...');
+  }
 }
 
 // const pers = new Person2();
@@ -39,56 +41,87 @@ class Person2 {
 // -----
 
 function Log(target: any, propetyName: string | Symbol) {
-    console.log('Property decorator!');
-    console.log(target, propetyName);
+  console.log('Property decorator!');
+  console.log(target, propetyName);
 }
 
 function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-    console.log('Accesor decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
+  console.log('Accesor decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
 }
 
-function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
-    console.log('Method decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log('Method decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
 }
 
 function Log4(target: any, name: string | Symbol, position: number) {
-    console.log('Parameter decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(position);
+  console.log('Parameter decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(position);
 }
-
 
 class Product {
-    @Log
-    title: string;
-    private _price: number;  
+  @Log
+  title: string;
+  private _price: number;
 
-    @Log2
-    set price(val: number) {
-        if (val > 0) {
-            this._price = val;
-        } else {
-            throw new Error("Invalid price - should be positive!"); 
-        }
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
     }
+  }
 
-    constructor(t: string, p: number) {
-        this.title = t;
-        this._price = p;
-    }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
 
-    @Log3
-    getPriceWithTax(@Log4 tax: number) {
-        return this._price * (1 + tax);
-    }
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
 }
 
-const p1 = new Product('Book 1', 19)
-const p2 = new Product('Book 2', 20)
+const p1 = new Product('Book 1', 19);
+const p2 = new Product('Book 2', 20);
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: true,
+    get() {
+      const foundFn = originalMethod.bind(this);
+      return foundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'This works!';
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+// button.addEventListener('click', p.showMessage.bind(p));
+button.addEventListener('click', p.showMessage);
